cfe877078efb14b7e80df5c36abf03c90000b584
Marta Łoskot <loskotmarta@gmail.com>
2024-06-19 17:34:17 +0200

    Fetch api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+
 const App = () => {
+
+  const getData = async() => {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    console.log(await response.json())
+  }
+  // nie ma potrzeby wywoływać getData(),bo ona już jest callback 
+  useEffect(() => getData, [])
+
   return (
     <div>
     </div>
