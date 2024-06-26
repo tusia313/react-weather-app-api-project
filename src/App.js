af927085eed4806bad31d0c300f2097e16874df1
Marta Łoskot <loskotmarta@gmail.com>
2024-06-26 18:34:26 +0200

    Introduction

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react"
-
 const App = () => {
 
-  const [countries, setCountries] = useState(null)
-
-  const getData = async () => {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    const json = await response.json()
-    setCountries(json)
-    console.log(json)
-  }
-  // nie ma potrzeby wywoływać getData(),bo ona już jest callback 
-  useEffect(() => getData, [])
-  // Mapowanie w kontekście programowania, a zwłaszcza w React, polega na przekształcaniu każdego elementu w tablicy na inny element. W React jest to często używane do generowania listy elementów JSX na podstawie danych.
   return (
     <div>
-      {countries?.map(country => <h3>{country.name.common}</h3>)}
     </div>
   )
 }
