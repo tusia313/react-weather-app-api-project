fed065b0b74ab86f7ff5d7d55643b98bd40180a6
Marta Łoskot <loskotmarta@gmail.com>
2024-06-27 16:58:35 +0200

    Getting current location

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+import { useState } from 'react'
 import TodayDisplay from './components/TodayDisplay'
 import Card from './components/Card'
 import UnitContainer from './components/UnitContainer'
 
 const App = () => {
 
+  const [position, setPosition] = useState(null)
+  const [error, setError] = useState(null)
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setError('Location API is not supported by Your browser.')
+    } else {
+      navigator.geolocation.getCurrentPosition(position => {
+        setPosition(position.coords)
+      },
+      // okej, to jest super ciekawy motyw, nie spotkałam się z czymś takim jeszcze
+        () => {
+          setError('Sorry, we cannot find Your location.')
+        }
+      )
+    }
+  }
+
+  getLocation()
+
+  // const fetchData = () => {
+
+  // }
+
   return (
     <div className="weather-app">
-      <TodayDisplay/>
+      <TodayDisplay />
       <div className="cards-container">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
-      <UnitContainer/>
+      <UnitContainer />
+      {error}
     </div>
   )
 }
