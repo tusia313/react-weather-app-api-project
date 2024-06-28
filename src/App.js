c469c8fd89fcc478987e61d3d54793365cdd471d
Marta Łoskot <loskotmarta@gmail.com>
2024-06-28 11:26:32 +0200

    Working with the API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodayDisplay from './components/TodayDisplay'
 import Card from './components/Card'
 import UnitContainer from './components/UnitContainer'
 
 const App = () => {
 
-  const [position, setPosition] = useState(null)
-  const [error, setError] = useState(null)
+  const [location, setLocation] = useState(null)
+  const [errorMessage, setError] = useState(null)
 
   const getLocation = () => {
     if (!navigator.geolocation) {
       setError('Location API is not supported by Your browser.')
     } else {
       navigator.geolocation.getCurrentPosition(position => {
-        setPosition(position.coords)
+        setLocation(position.coords)
       },
-      // okej, to jest super ciekawy motyw, nie spotkałam się z czymś takim jeszcze
+        // okej, to jest super ciekawy motyw, nie spotkałam się z czymś takim jeszcze
         () => {
           setError('Sorry, we cannot find Your location.')
         }
@@ -23,11 +23,18 @@ const App = () => {
     }
   }
 
-  getLocation()
-
-  // const fetchData = () => {
-
-  // }
+  const fetchData = () => {
+    const longitude = location?.longitude
+    const latitude = location?.latitude
+    fetch(`http://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=astro&output=xml`)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.error(err))
+  }
+  useEffect(() => {
+    getLocation()
+      fetchData()
+  }, [])
 
   return (
     <div className="weather-app">
@@ -42,7 +49,7 @@ const App = () => {
         <Card />
       </div>
       <UnitContainer />
-      {error}
+      {errorMessage}
     </div>
   )
 }
